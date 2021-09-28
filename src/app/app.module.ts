fa373491a23de62a83f6b44f42cb55632ba63462
PanoramaView <zhou-1997@hotmail.it>
2021-09-28 16:13:14 +0200

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostDetailComponent } from './posts/post-detail/post-detail.component';
+import { PostShowComponent } from './posts/post-list/post-show/post-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PostsComponent,
+    PostListComponent,
+    PostDetailComponent,
+    PostShowComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+// export class PostListComponent implements OnInit {
+//     // @Output() postWasSelected = new EventEmitter<post>(); removed after service injection
+//     posts: Post[]; /* empty cuz now the data are in the post.service, that will be injected */
+
+//     /* inject our Service */
+//     constructor(private postService: PostService,
+//       private router: Router,
+//       private route: ActivatedRoute) { }
+
+//   ngOnInit(): void {
+//   }
+// }
+//   ngOnInit() {
+//     this.PostService.postsChanged
+//         .subscribe(
+//           (posts: Post[]) => {
+//             this.posts = posts;
+//           }
+//         )
+//     this.posts = this.PostService.getposts();
+//   }
+
+//   /* onpostSelected(post: post){
+//     this.postWasSelected.emit(post);
+//     removed after service injection
+//   } */
+
+//   onNewPost(){
+//     this.router.navigate(['new'], {relativeTo: this.route})
+//   }
+// }
